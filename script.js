bb0bbc915e6f6c5d52628becaf911f2bef63be28
hafsahussein <49996540+hafsahussein@users.noreply.github.com>
2019-05-15 11:03:38 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 $(function() {
 var guess= Math.floor(Math.random()*(100))+1;
 var countingClick=0;
+var attempts=[];
 $('form').submit(function() {
 var val=$('#guess').val();
 if(val>100|| val<1)
@@ -9,17 +10,19 @@ if(val>100|| val<1)
 }
 else
 {
+attempts.push(val);
 if(Number(val)===guess) {
 	$('p').eq(0).text("You Did it");
 	$('p').eq(0).css('background-color','green');
 }
 
 else if(Number(val)>guess) {
-	$('p').eq(0).text("Go Down ");
+	$('p').eq(0).html("Go Down+<br/>last attempts:"+attempts);
 	countingClick++;
 	}
 else{
-	$('p').eq(0).text("Go up")
+	$('p').eq(0).html("Go up+<br/>last attempts:"+attempts);
+")
 		countingClick++;
 
 	}
@@ -31,6 +34,7 @@ else{
 	$('#guess').val('');
 		$('p').eq(0).text("");
 		countingClick=0;
+                attempts=[];
 	}
 })
 
@@ -38,6 +42,7 @@ $('button').eq(0).click(function() {
 	guess=Math.floor(Math.random()*(100))+1;
 	$('#guess').val('');
 		$('p').eq(0).text("");
+                attempts=[];
 
 })
 })
